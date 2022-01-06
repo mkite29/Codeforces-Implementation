27491d96fd79ad34081e52a29f51a89e591cf6f0
Mohit Kaimwaal <72551003+mkite29@users.noreply.github.com>
2022-01-06 18:51:10 +0530

    Update Integer Diversity.cpp

--- Integer Diversity.cpp
+++ Integer Diversity.cpp
@@ -5,20 +5,21 @@ void solve(){
  
 	vector<int> arr(n);
 	unordered_map<int,int> mp;
- 
+ 	
+	//phle sab ka abs le lia 
 	for(int i = 0 ; i < n ; i++){
 		cin>>arr[i];
 		mp[abs(arr[i])]++;
 	}
  
 	int res = 0;
- 
+ 	//and ager map mei vo number ek baar aya hai use 1 baar hi le sakte hai
 	for(auto i : mp){
  
 		if(i.second == 1){
 			res++;
 		}
- 
+ 		//ager do baar aya hai to use 2 tarike se le sakte hai ek khud and ek negative ban ker.
 		else {
 			res+=2;
 			if(i.first == 0){
